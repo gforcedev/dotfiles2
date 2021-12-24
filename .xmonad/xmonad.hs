cdab522fd6805e2fc3ecf135310e80f4b1ad27c2
Gforcedev <tecshotclark@gmail.com>
2021-12-24 23:10:06 +0000

    Add a minimum working xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,30 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import System.Exit
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+		, handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+		, terminal = "st"
+		, borderWidth = 15
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+		, ((mod4Mask, xK_Return), spawn "st")
+		, ((mod4Mask .|. shiftMask, xK_q), kill)
+		, ((mod4Mask .|. controlMask .|. shiftMask, xK_BackSpace), io (exitWith ExitSuccess))
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
