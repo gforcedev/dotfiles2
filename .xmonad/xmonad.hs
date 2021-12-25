4bb47806d41f7a7c346e31cbed74a0f6a9c131c7
Gforcedev <tecshotclark@gmail.com>
2021-12-25 14:13:08 +0000

    Do some more desktop ricing

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,31 +1,37 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import System.Exit
 
+myStartupHook = do
+    spawnOnce "picom"
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-		, handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+        , layoutHook = gaps [(U,35)] $ spacingWithEdge 10 $ avoidStruts  $  layoutHook defaultConfig
+        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-		, terminal = "st"
-		, borderWidth = 15
+        , terminal = "st"
+        , borderWidth = 0
+        , startupHook = myStartupHook
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-		, ((mod4Mask, xK_Return), spawn "st")
-		, ((mod4Mask .|. shiftMask, xK_q), kill)
-		, ((mod4Mask .|. shiftMask, xK_x), spawn "betterlockscreen -l")
-		, ((mod4Mask .|. controlMask .|. shiftMask, xK_BackSpace), io (exitWith ExitSuccess))
+        , ((mod4Mask, xK_Return), spawn "st")
+        , ((mod4Mask .|. shiftMask, xK_q), kill)
+        , ((mod4Mask .|. shiftMask, xK_x), spawn "betterlockscreen -l")
+        , ((mod4Mask .|. controlMask .|. shiftMask, xK_BackSpace), io (exitWith ExitSuccess))
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         ]
