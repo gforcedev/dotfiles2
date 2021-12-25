6affe77f6c557f7f1c06146818f99cefbcfaa791
Gforcedev <tecshotclark@gmail.com>
2021-12-25 12:47:36 +0000

    Add picom config, add betterlockscreen binding

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -24,6 +24,7 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
 		, ((mod4Mask, xK_Return), spawn "st")
 		, ((mod4Mask .|. shiftMask, xK_q), kill)
+		, ((mod4Mask .|. shiftMask, xK_x), spawn "betterlockscreen -l")
 		, ((mod4Mask .|. controlMask .|. shiftMask, xK_BackSpace), io (exitWith ExitSuccess))
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
